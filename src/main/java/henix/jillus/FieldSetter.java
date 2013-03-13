65a09507bb119ff70a8f5369fd6fe8924c2b661c
henix <shellpick@gmail.com>
2013-03-13 16:00:50 +0800

    improve comment of FieldSetter

--- src/main/java/henix/jillus/FieldSetter.java
+++ src/main/java/henix/jillus/FieldSetter.java
@@ -3,7 +3,7 @@ package henix.jillus;
 public interface FieldSetter<T, V> {
 
 	/**
-	 * Set some obj's field to value
+	 * Set one of obj's field to value
 	 */
 	public void setValue(T obj, V value);
 }
